c59bd3ec569d82359a6dd619d856fba6a855f174
pbrudnick <pbrudnick@gmail.com>
2016-11-16 21:54:58 -0300

    removing user stuff

--- web/controllers/user_controller.ex
+++
@@ -1,7 +0,0 @@
-defmodule PyconarTalks.UserController do
-  use PyconarTalks.Web, :controller
-
-  def new(conn, params, current_user, _claims) do
-    render conn, "new.html", current_user: current_user
-  end
-end
