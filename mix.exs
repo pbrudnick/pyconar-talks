c97471b3ddafc0e09492c726392cbd96ac06e5d3
pbrudnick <pbrudnick@gmail.com>
2016-11-20 11:13:19 -0300

    setting v0.1.0 of the app

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule PyconarTalks.Mixfile do
 
   def project do
     [app: :pyconar_talks,
-     version: "0.0.1",
+     version: "0.1.0",
      elixir: "~> 1.2",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
